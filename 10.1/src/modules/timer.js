054e61cad21a7734136046c0b5f3506dc07a9ae9
z0r0v <3767082@mail.ru>
2019-06-03 21:33:47 +0300

    fix bug in timer

--- 10.1/src/modules/timer.js
+++ 10.1/src/modules/timer.js
@@ -11,7 +11,6 @@ htmlElements.buttonStop.addEventListener('click', timerStopButton);
 
 
 let difference;
-let startTime;
 let startInterval;
 
 function defaultTime(){
@@ -27,20 +26,21 @@ function defaultTime(){
 defaultTime();
 
 function runTimer() {
-    difference = difference - (new Date().getTime() - startTime) / 1000;
-    let seconds = parseInt(difference % 60);
-    let minutes = parseInt((difference / 60) % 60);
-    let hours = parseInt(((difference / 60) % 60)/60 % 60);
+    let newDifference = --difference;
+    let seconds = parseInt(newDifference % 60);
+    let minutes = parseInt((newDifference / 60) % 60);
+    let hours = parseInt(((newDifference / 60) % 60)/60 % 60);
     if(seconds < 10){seconds = '0' + seconds;}
     if(minutes < 10){minutes = '0' + minutes;}
     if(hours < 10){hours = '0' + hours;}
     htmlElements.output.innerText = `${hours}:${minutes}:${seconds}`; 
-    }
-
+    if(newDifference === 0){
+        clearInterval(startInterval);
+    } 
+}
 
 function startTimer(){
     startInterval = setInterval(runTimer, 1000);
-    startTime = new Date().getTime();
     htmlElements.buttonStart.setAttribute('disabled', 'disabled');
 }
 
