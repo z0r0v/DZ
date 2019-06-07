01b889eb0c720017baae21a6bd7acb09fc3ec278
Alex Zhorov <3767082@mail.ru>
2019-06-07 11:22:37 +0300

    add 11 recursion

--- 11/src/modules/main.js
+++ 11/src/modules/main.js
@@ -1,13 +1,18 @@
-function onButtonClicked(){
-    if (checkPassword() === true){
-      alert("OK");
-    }
+const htmlButton = document.querySelector('button');
+htmlButton.addEventListener('click',onButtonlogInClicked);
+
+function onButtonlogInClicked(){
+  if(checkPassword() === true){
+    alert('OK');
   }
-  
-  function checkPassword(){
-    let a = prompt("password");
-    if (a !== 'mypassword'){
-      return checkPassword();
-    }
+}
+
+function checkPassword(){
+  let pasword = prompt('enter pasword');
+  if(pasword !== 'q1w2e3'){
+    return checkPassword()
+  }
+  else{
     return true;
   }
+}
