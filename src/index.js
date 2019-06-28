047976e30b75f3175b7157536dae329000ce5e91
Alex Zhorov <3767082@mail.ru>
2019-06-28 18:20:14 +0300

    upload general index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './style.css';
 import './modules/main.js';
 
-import './css/default.css';
+// import './css/default.css';
 import './css/header.css';
 import './css/portfolio.css';
 import './css/tasks.css';
 import './css/footer.css';
 import './css/skils.css';
+import './css/experience.css';
