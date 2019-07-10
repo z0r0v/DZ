beca9c0bb09f38fe9d7e9871589490a717f73fe5
Alex Zhorov <3767082@mail.ru>
2019-07-10 13:35:05 +0300

    upload img

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 import './modules/main.js';
 
-// import './css/default.css';
+
 import './css/header.css';
 import './css/portfolio.css';
 import './css/tasks.css';
 import './css/footer.css';
 import './css/skils.css';
 import './css/experience.css';
+import './css/slider.css';
+
