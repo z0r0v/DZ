5ea9225e2b17a2b848a8f2284f747c27857a07d7
Alex Zhorov <3767082@mail.ru>
2019-07-11 18:34:25 +0300

    upload css

--- 18.1/src/modules/main.js
+++ 18.1/src/modules/main.js
@@ -3,7 +3,7 @@ import {Slider} from './slider.js'
 const slider = new Slider();
 const newLogin = new Login();
 
-// import { Book } from "./book.js";
-// const newBook = new Book();
+import { Book } from "./book.js";
+const newBook = new Book();
 
 
