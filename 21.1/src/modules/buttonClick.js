0e2e61275f168e7476368375c0a7eb8d2afa43aa
Alex Zhorov <3767082@mail.ru>
2019-07-16 16:17:26 +0300

    upload addEventListener

--- 21.1/src/modules/buttonClick.js
+++ 21.1/src/modules/buttonClick.js
@@ -1,10 +1,17 @@
+import log from './logger.js'
+
 const htmlElements = {
   button:document.querySelector("#button"),
 };
-(()=>{
+
+const addEventListener = (name) => {
   const before = new Date().getTime();
   htmlElements.button.addEventListener('click', doSomething);
+
   const doSomething = () => {
-  
+    const after = new Date().getTime();
+    log(name, before, after);
   }
-})
+};
+
+export {addEventListener};
