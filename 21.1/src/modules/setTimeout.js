6fd06f57553ab5789cdfb0e98330a5261c0458ba
Alex Zhorov <3767082@mail.ru>
2019-07-16 16:56:48 +0300

    upload 21.1

--- 21.1/src/modules/setTimeout.js
+++ 21.1/src/modules/setTimeout.js
@@ -1,13 +1,13 @@
-import log from './logger.js'
-const runSetTimeOut = (name, milliseconds)=>{
+import log from "./logger.js";
+
+const runSetTimeOut = (name, milliseconds) => {
   const before = new Date().getTime();
 
-  const getLeadTime = ()=>{
+  const getLeadTime = () => {
     const after = new Date().getTime();
     log(name, before, after, ` putative: ${milliseconds};`);
-  }
+  };
 
   setTimeout(getLeadTime, milliseconds);
-
-}
-export {runSetTimeOut};
+};
+export { runSetTimeOut };
--- 21.1/src/modules/xmlHttpRequest.js
+++ 21.1/src/modules/xmlHttpRequest.js
@@ -4,7 +4,6 @@ const runXhr = (name, url) => {
   const before = new Date().getTime();
   const xmr = new XMLHttpRequest();
   xmr.open("GET", url);
-
   xmr.onload = function() {
     const after = new Date().getTime();
     log(name, before, after);
