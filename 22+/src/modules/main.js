4f21e85aaa76ebd2283d4ec3965e43609042253a
Zhorov Alex <zhorovalex@MacBook-Air-Anna.local>
2019-07-20 17:20:12 +0300

    upload 22+

--- 22+/src/modules/main.js
+++ 22+/src/modules/main.js
@@ -1,26 +1,16 @@
-
-const provideParameters = (url) =>{
-
+const provideParameters = function(url){
   const flinders = url.split('?');
   const result = {};
-
-  if(flinders.lenght === 2) {
-
+  if(flinders.length === 2) {
     const partRight = flinders[1];
-
-    const couple = partRight.split('&');
-
-    couple.forEach(element => {
-     
-      const detailsCouple = element.split('=');
+    const cours = partRight.split('&');
+    cours.forEach(cour => {
+      const detailsCouple = cour.split('=');
       const name = detailsCouple[0];
       const value = detailsCouple[1];
       result[name] = value;
-
     });
-    console.log(element);
   }
 console.log(result);
-}
-
+};
 provideParameters('https://www.youtube.com/watch?v=aUa2gaI3LQI&feature=share');
