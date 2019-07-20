b693f9fec019fa06d853046494fd9adccd0e5e27
Alex Zhorov <3767082@mail.ru>
2019-07-20 13:42:15 +0300

    upload 22.1.2

---
+++ 22.2/src/modules/main.js
@@ -0,0 +1,9 @@
+
+const htmlElements = {
+  myLink:document.querySelector('.myLink'),
+}
+
+htmlElements.myLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = "http://google.com";
+})
