bf0af8b1fe46cdb54f996f39eb56ae06933840c5
Zhorov Alex <zhorovalex@MacBook-Air-Anna.local>
2019-07-24 19:56:19 +0300

    uplodad 23.1

--- 23.1/src/modules/main.js
+++ 23.1/src/modules/main.js
@@ -1,13 +1,24 @@
-function delay(ms){
-  return Promise.resolve().then( () => {
-    return new Promise(function(resolve, reject){
-      setTimeout(resolve(), ms);
-    })
-  })
-};
+function delay(ms) {
+  return Promise.resolve().then(() => {
+    return new Promise(function(resolve, reject) {
+      setTimeout(resolve.bind(this, ms), ms);
+    });
+  });
+}
+
+function delay2(ms) {
+  return Promise.resolve().then(() => {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => {
+        resolve.call(this, ms);
+      }, ms);
+    });
+  });
+}
 
 delay(1000).then(function(ms) {
-  console.log(ms);
+  console.log("delay_1000:", ms);
 });
-
-
+delay2(3000).then(function(ms) {
+  console.log("delay_3000:", ms);
+});
