c1c0a7f8fb11c05f529f219eda00e7eee162172d
Alex Zhorov <3767082@mail.ru>
2019-07-26 08:57:28 +0300

    uplodad 24.1

--- 24.2/src/modules/main.js
+++ 24.2/src/modules/main.js
@@ -1,44 +1,38 @@
-class URLRarser{
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const provideParameters = () => {
-  const flinders = url.split('?');
-  const result = {};
-  if(flinders.length === 2) {
-    const partRight = flinders[1];
-    const cours = partRight.split('&');
-    cours.forEach(cour => {
-      const detailsCouple = cour.split('=');
-      const name = detailsCouple[0];
-      const value = detailsCouple[1];
-      result[name] = value;
-    });
-    return result;
+class URLParser {
+  constructor(url = window.location.href) {
+    this.url = url;
   }
+
+  provideParametersRegExp = () =>{
+    const regExp = new RegExp('[?&]+([^=&]+)=([^&]*)', 'g');
+    const result = {};
+    this.url.replace(regExp, (match, key, value) => {
+      result[key] = value;
+    });
+  return result;
+  };
+
+  provideParametersViaSplit = () => {
+    const flinders = this.url.split('?');
+    const result = {};
+    if(flinders.length === 2) {
+      const partRight = flinders[1];
+      const cours = partRight.split('&');
+      cours.forEach(cour => {
+        const detailsCouple = cour.split('=');
+        const name = detailsCouple[0];
+        const value = detailsCouple[1];
+        result[name] = value;
+      });
+      return result;
+    }
+  };
 };
 
+
 const urlParser = new URLParser('http://127.0.0.1:5500?a=1&b=&c=3');
+console.log(urlParser.provideParametersRegExp());
+console.log(urlParser.provideParametersViaSplit());
 
 
-console.log(object);
+
