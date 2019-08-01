95b62cde8f9b36ecc4c46f011159151be7cd8145
Alex Zhorov <3767082@mail.ru>
2019-08-01 17:29:13 +0300

    upload project reexport

--- 25.1/src/modules/reexport.js
+++ 25.1/src/modules/reexport.js
@@ -1,30 +1,20 @@
 export { UserServiceFetch, UserServiceHXMhttp } from "./serviceServer/user.service.js";
 export { ClassHelper } from "./helpModule/classHelper.js";
 export { Clock } from "./clock.js";
-
 export { masters } from "./dataBase/materDataBase.js";
 export { books, Book} from "./dataBase/carDatabase.js";
 export { carsOwners, CarsOwnersCeate } from "./dataBase/carOwners.js";
-
 export { AutoInfoGetOder } from "./autoInfoGetOder.js";
-
 export { infoCar, infoOrder, BooksTable, RenderBook } from "./book.js";
 export { StopWotch } from "./stopWotch.js";
 export { apendHelpper } from "./helpModule/apendHelpper.js";
-
 export { Loggin } from "./login.js";
-
-
 export { infoBook } from "./infoBook.js";
-
-
 export {Slider} from './slider.js';
 export {Tabs} from './tabs.js';
 export { SwitchCase, SwitchCase2 } from "./helpModule/switchCase.js";
-
 export { ValidationForm } from "./validation.js";
 export { MaskPhone } from "./helpModule/maskPhone.js";
-
 export { AutoInfo } from "./autoInfo.js";
 
 
